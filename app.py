1636fca9203070f68898a6c35ff1e3ba433b9f0a
Karim <kk9768004@gmail.com>
2022-02-21 11:22:46 +0530

    Changes done

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from resources.store import Store, StoreList
 
 
 app =  Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIICATIONS'] = False
 app.secret_key = "salman"
 api = Api(app)
