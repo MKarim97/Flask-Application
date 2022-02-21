7ab77c69cbb3e6ab69994e3572270a5ea9a25790
Karim <kk9768004@gmail.com>
2022-02-21 10:38:31 +0530

    Added a PostgreSQL environment varialbe.

--- app.py
+++ app.py
@@ -12,12 +12,10 @@ from resources.store import Store, StoreList
 
 
 app =  Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIICATIONS'] = False
 app.secret_key = "salman"
 api = Api(app)
-
-
 jwt = JWT(app,authenticate,identity)
 
 # items = []
@@ -33,3 +31,4 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
     app.run(debug=True)
+    
